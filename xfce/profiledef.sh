2047fb56e2501a0ec1aa5918d4a3e296278ed0d5
Will Forsberg <forsbergw82@gmail.com>
2020-09-12 13:52:23 -0500

    fix profiledef.sh

--- xfce/profiledef.sh
+++ xfce/profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="archlive-xfce"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://www.archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
